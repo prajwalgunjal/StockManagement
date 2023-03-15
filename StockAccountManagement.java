42a7b7d28d3eb1c068e54f56ddb5fef85d7a56ff
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-03-15 23:15:49 +0530

    Added:Stock

---
+++ StockAccountManagement.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.stockmanagement;
+
+public class StockAccountManagement {
+
+    public static void main(String[] args) {
+
+        System.out.println("******Welcome to the Stock Account Management Program******");
+
+        StockPortfolio portfolio = new StockPortfolio();
+        portfolio.selectChoice();
+
+    }
+}
